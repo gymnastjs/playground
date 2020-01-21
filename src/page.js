51afe3db6c58264f5d5a9aa90da358f6bc52ed2f
Oscar <obartra@gmail.com>
2020-01-20 19:38:26 -0800

    chore(npm): update to latest gymnast
    Update demo to current version of gymnast

--- src/page.js
+++ src/page.js
@@ -1,70 +1,68 @@
 import React from "react";
-import {
-  Grid,
-  Layout,
-  Root,
-  Col,
-  ConfigProvider,
-  Dev,
-  asLayout
-} from "gymnast";
+import { Grid, Col, GymnastProvider, useGrid } from "gymnast";
 
 const boxStyle = {
   color: "#1469cc",
   backgroundColor: "#efefef"
 };
+
 const style = {
+  HEADER: { position: "fixed", align: "top" },
   TALL: { ...boxStyle, minHeight: 200 },
   MEDIUM: { ...boxStyle, minHeight: 150 },
   SMALL: { ...boxStyle, minHeight: "initial" }
 };
 
-const Footer = asLayout("footer");
-const Header = asLayout("header");
+function Footer(props) {
+  const [shouldRender, allProps] = useGrid(props);
+
+  if (!shouldRender) {
+    return null;
+  }
+
+  return <footer {...allProps} />;
+}
+
+function Header(props) {
+  const [shouldRender, allProps] = useGrid(props);
+
+  if (!shouldRender) {
+    return null;
+  }
+
+  return <header {...allProps} />;
+}
 
 export default function Page() {
   return (
-    <ConfigProvider>
-      <Dev />
-      <Layout height="parent" className="page">
-        <Header fixed="top">
-          <Root>
-            <Col marginBottom={0}>
-              <h1>Header</h1>
-            </Col>
-          </Root>
-        </Header>
-        <Layout height="parent" marginTop={7.5}>
-          <Layout height="auto">
-            <Root>
-              <Grid marginBottom={0} marginTop="L">
-                <Col size={10} style={style.TALL} />
-                <Col size={2} style={style.SMALL} />
+    <GymnastProvider>
+      <Header style={style.HEADER}>
+        <Col marginBottom={0}>
+          <h1>Header</h1>
+        </Col>
+      </Header>
+      <Grid marginTop="2XL" paddingTop="L">
+        <Col size={10} style={style.TALL} />
+        <Col size={2} style={style.SMALL} />
 
-                <Col style={style.MEDIUM} />
+        <Col style={style.MEDIUM} />
 
-                <Col size={6} style={style.MEDIUM} />
-                <Col size={6} style={style.MEDIUM} />
+        <Col size={6} style={style.MEDIUM} />
+        <Col size={6} style={style.MEDIUM} />
 
-                <Col size={4} style={style.TALL} />
-                <Col size={4} style={style.TALL} />
-                <Col size={4} style={style.TALL} />
+        <Col size={4} style={style.TALL} />
+        <Col size={4} style={style.TALL} />
+        <Col size={4} style={style.TALL} />
 
-                <Col size={4} style={style.TALL} />
-                <Col size={4} style={style.TALL} />
-                <Col size={4} style={style.TALL} />
-              </Grid>
-            </Root>
-          </Layout>
-          <Footer>
-            <Root>
-              <Col>
-                <h1>Footer</h1>
-              </Col>
-            </Root>
-          </Footer>
-        </Layout>
-      </Layout>
-    </ConfigProvider>
+        <Col size={4} style={style.TALL} />
+        <Col size={4} style={style.TALL} />
+        <Col size={4} style={style.TALL} />
+      </Grid>
+      <Footer>
+        <Col>
+          <h1>Footer</h1>
+        </Col>
+      </Footer>
+    </GymnastProvider>
   );
 }
